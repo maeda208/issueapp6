88116584aabe4cad7b8dd5cdff495cbcff42ee8c
maeda208 <haneumariderbypornograffitti@yahoo.co.jp>
2024-08-18 18:21:24 +0900

    Initial commit

---
+++ src/main/java/in/techcamp/issueapp6/Issueapp6Application.java
@@ -0,0 +1,13 @@
+package in.techcamp.issueapp6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Issueapp6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Issueapp6Application.class, args);
+	}
+
+}
---
+++ src/test/java/in/techcamp/issueapp6/Issueapp6ApplicationTests.java
@@ -0,0 +1,13 @@
+package in.techcamp.issueapp6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Issueapp6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
